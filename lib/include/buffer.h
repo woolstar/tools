d7edb2fe85d4719cae038147f503a908271c6463
woolstar <jdwoolverton@gmail.com>
2013-09-23 12:00:15 -0700

    rearrange buffers around new hiearchy of Policy classes

--- lib/include/buffer.h
+++ lib/include/buffer.h
@@ -1,11 +1,11 @@
-#ifndef LTL_BUFFER_
-#define LTL_BUFFER_ 1
+#ifndef BTL_BUFFER_
+#define BTL_BUFFER_ 1
 
 #include <cstddef>
 #include <stdexcept>
 #include <memory>
 
-namespace ltl
+namespace btl
 {
 	class	buffer
 	{
@@ -23,43 +23,63 @@ namespace ltl
 			size_t	datasize_ { 0 } ;
 	} ;
 
-	class	build : public buffer
+	class	build_base
 	{
 		public:
-			build(unsigned char * astorage, int amax) :
-				buffer( astorage, 0), fill_(astorage), limit_(astorage + amax) { }
-
-			build &	add(const buffer &) ;
-			build &	add(const char * astr) ;
-
-			build &	add_u8(unsigned char) ;
-			build &	add_u16(unsigned short) ;
-			build &	add_u32(unsigned) ;
-			build &	add_u64(unsigned long long int) ;
-
-			build &	chomp() ;
-			void	terminate() ;
+			build_base(unsigned char * aptr, size_t amax) : buffer(aptr, 0), fill_(aptr), limit_(aptr + amax) { }
+			virtual ~ build_base() ;
 
 		protected:
-			virtual bool	expand(int aspace) = 0 ;
-
-			template <int abits> friend void add(build &, unsigned int aval) ;
-
 			unsigned char * fill_, * limit_ ;
 	} ;
 
-
-	template <int abits> void add(build &, unsigned int aval) ;
-
-	template <> void add<8>(build & abuf, unsigned int aval) { abuf.add_u8( aval) ; }
-	template <> void add<16>(build & abuf, unsigned int aval) { abuf.add_u16( aval) ; }
-	template <> void add<32>(build & abuf, unsigned int aval) { abuf.add_u32( aval) ; }
-
-	template <int asize, typename aTYPE = unsigned char>
-		class build_static : public build
+	template <class ExpansionTrait, typename PrimativeStorage = unsigned char>
+		class	build_methods : public build_base, public ExpansionTrait
 		{
 			public:
-				build_static() : build(reinterpret_cast<unsigned char *>(&storage_[0]), sizeof(storage_)) { }
+				build_methods &	add(const buffer &) ;
+				build_methods &	add(const char * astr) ;
+
+				build_methods &	add_u8(unsigned char) ;
+				build_methods &	add_u16(unsigned short) ;
+				build_methods &	add_u32(unsigned) ;
+				build_methods &	add_u64(unsigned long long int) ;
+
+				build_methods &	chomp() ;
+				build_methods &	terminate() ;
+
+			protected:
+				template <int abits> friend void add(build &, unsigned int aval) ;
+
+				unsigned char * fill_, * limit_ ;
+		} ;
+
+
+	template <int abits> void add(build_methods &, unsigned int aval) ;
+
+	template <> void add<8>(build_methods & abuf, unsigned int aval) { abuf.add_u8( aval) ; }
+	template <> void add<16>(build_methods & abuf, unsigned int aval) { abuf.add_u16( aval) ; }
+	template <> void add<32>(build_methods & abuf, unsigned int aval) { abuf.add_u32( aval) ; }
+
+		// traits
+
+			// keep buffer restricted to initial size
+	class	build_fixed : virtual public build_base
+		{ protected: bool expand(int) { return false ; } } ;
+
+			// exception on buffer overflow
+	class	build_strict : virtual public build_base
+		{ protected: bool expand(int) { throw std::length_error("exceeded buffer") ; } } ;
+
+			// attempt to reallocate larger buffer each time
+	class	build_expand : virtual public build_base
+		{ protected: bool expand(int) ; } ;
+
+	template <int asize, typename aTYPE = unsigned char>
+		class build_static : public build_methods<build_fixed>
+		{
+			public:
+				build_static() : build_base(reinterpret_cast<unsigned char *>(storage_), sizeof(storage_)) { }
 				build_static(const buffer &) ;
 				build_static(char const * astr, size_t, char aencoding) ;
 
@@ -71,39 +91,19 @@ namespace ltl
 
 	//
 
-	class	build_resource : public build
+	class	build_managed : public build_methods<build_expand>
 	{
 		public:
-			build_resource(size_t asize = 1024) ;
-			build_resource(const buffer & ) ;
+			build_managed(size_t asize = 1024) ;
+			build_managed(const buffer & ) ;
 
-			build_resource(build_resource &&) ;
-			build_resource(build_resource &) = delete ;
+			build_managed(build_managed &&) ;
+			build_managed(build_managed &) = delete ;	// use the buffer constructor instead
+
+			build_managed& operator=(build_managed &&) ;
 
 		protected:
 			std::unique_ptr<unsigned char *>	storage_ ;
-	} ;
-
-		// traits
-
-			// keep buffer restricted to initial size
-	class	build_fixed : virtual public build
-		{ protected: bool	expand(int) { return false ; } } ;
-
-			// exception on buffer overflow
-	class	build_strict : virtual public build_fixed
-		{ protected: bool	expand(int) { throw std::length_error("exceeded buffer") ; } } ;
-
-	class	build_expand : public build
-	{
-		public:
-			build_expand(int ainitialsize = 1024) ;
-			build_expand(const buffer &) ;
-
-			~ build_expand() ;  // ... deallocate
-
-		protected:
-			// smart pointer holder
 	} ;
 
 #if 0
