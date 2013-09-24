4ac3b5b038bdbd6c9150fe909211d420f04cde95
woolstar <jdwoolverton@gmail.com>
2013-09-23 23:36:33 -0700

    use bufferscanner to implement copy

--- lib/buffer/build.cpp
+++ lib/buffer/build.cpp
@@ -1,7 +1,16 @@
 
 #include <buffer.h>
+#include <buffer_scanner.h>
 
 using	btl::build_base ;
+using	btl::scanner ;
 
 build_base:: ~ build_base() { }
+
+void	build_base::copy(buffer const & abuf)
+{
+	scanner<sized_storage>	scan( abuf) ;
+
+	while ( scan.size_remaining() && ( fill_ < limit_ ) ) { *( fill_ ++)= * (scan ++ ) ; }
+}
 
