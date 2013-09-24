eea8a0043667a8681e6d1703f0b2ab6f9ca0e89c
woolstar <jdwoolverton@gmail.com>
2013-09-24 14:46:40 -0700

    forward declaration of base types for BTL

---
+++ lib/include/bits/btl_basetypes.h
@@ -0,0 +1,24 @@
+#ifndef _BTL_BASETYPES_H
+#define _BTL_BASETYPES_H 1
+
+namespace btl
+{
+		// buffer
+	class	buffer ;
+	template <class T> class	scanner ;
+
+		// data
+	class	source ;
+	class	chain ;
+
+		// container
+	template <class T> class	slist ;
+	template <class T> class	list ;
+	template <class T> class	aatree ;
+
+		// io
+	class io ;
+} ;
+
+#endif
+
