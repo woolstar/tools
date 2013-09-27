cccc111b98cb01f2f9f38bc941a64fc8a4f2aeac
woolstar <jdwoolverton@gmail.com>
2013-09-26 23:03:12 -0700

    io class

---
+++ lib/include/bits/btl_iocore.h
@@ -0,0 +1,47 @@
+// io core -*- C++ -*-
+
+#ifndef _BTL_IOCORE_H
+#define _BTL_IOCORE_H 1
+
+namespace btl
+{
+	typedef int	IO_Port ;
+
+	class	ioset ;
+
+	class	io
+	{
+		public:
+			io(IO_Port) ;
+			virtual ~ io() ;
+
+				// specialization
+			virtual int	doread(void) = 0 ;
+			virtual bool	dowrite(void) { return false ; }
+
+				// io operators
+			virtual void	close(void) ;
+			virtual int	read(build_base &) const ;
+			virtual int	print(const buffer &) const ;
+
+				// test active
+			bool	isactive(void) const { return active_ ; }
+
+		protected:
+			io() : port_( 0) {}
+
+			void	setup(IO_Port) ;
+
+			friend class	ioset ;
+
+			virtual IO_Port	getPort() const { return port_ ; }
+
+			IO_Port	port_ ;
+			bool	active_ = false ;
+			ioset	* set_ = nullptr ;
+	} ;
+
+} ;
+
+#endif
+
