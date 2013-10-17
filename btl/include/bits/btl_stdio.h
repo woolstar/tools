dff9c83b7f8816caef072cbb3348f8507f45fd8d
woolstar <jdwoolverton@gmail.com>
2013-10-16 23:02:02 -0700

    basic console io that knows about buffers

---
+++ btl/include/bits/btl_stdio.h
@@ -0,0 +1,34 @@
+// stdio -*- C++ -*-
+
+#ifndef _BTL_STDIO_H
+#define _BTL_STDIO_H 1
+
+namespace btl
+{
+
+	class	ioread : public io
+	{
+		public:
+			ioread( IO_Port ) ;
+			int	doread(void) ;
+	} ;
+
+	class	iowrite : public io
+	{
+		public:
+			iowrite( IO_Port ) ;
+			int	doread(void) { return 0 ; }
+			bool	dowrite(void) ;
+
+			int	read(build_base &) const ;
+	} ;
+
+	extern ioread	ioin ;
+	extern iowrite	ioout ;
+
+	static io::Init		__init ;
+
+} ;
+
+#endif
+
