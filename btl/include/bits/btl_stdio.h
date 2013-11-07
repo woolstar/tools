80997515d92fcc5089794d99f63dcd995bdbd91b
woolstar <jdwoolverton@gmail.com>
2013-11-07 00:34:57 -0700

    move the io operators into iobasic

--- btl/include/bits/btl_stdio.h
+++ btl/include/bits/btl_stdio.h
@@ -3,18 +3,30 @@
 #ifndef _BTL_STDIO_H
 #define _BTL_STDIO_H 1
 
+#include <unistd.h>
+
 namespace btl
 {
-
-	enum IODirection { eReadWrite, eRead, eWrite } ;
-
 	class	iobasic : public io
 	{
 		public:
-			iobasic( IO_Port, IODirection adir ) ;
-			iobasic() {}
+			iobasic( IO_Port ) ;
+			~ iobasic() ;
 
-			int	doread(void) { return 0 ; }
+				// io operators
+			int	read(build_base & zbuf) ;
+			int	print(const buffer & abuf) const ;
+			int	ctrl( int icode, void * aptr) const ;
+			void	close(void)
+				{
+					if ( active_ ) {
+						active_= false ;
+
+						::close( port_ ) ;
+					}
+				}
+
+			const iobasic&	operator<<(const buffer & abuf) const { print(abuf) ;  return * this ; }
 	} ;
 
 	extern iobasic	ioin ;
