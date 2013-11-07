40badcef5eb2804e60fdcf396e3b5c7ab97efbd2
woolstar <jdwoolverton@gmail.com>
2013-11-07 12:57:11 -0700

    reworked manage with PIMPL handlers

--- btl/include/bits/btl_manage.h
+++ btl/include/bits/btl_manage.h
@@ -12,9 +12,13 @@ namespace btl
 	class	manage 
 	{
 		public:
-			void	monitor( io * ) ;
-			void	push( io * ) ;
-			void	release( io * ) ;
+			enum Modes
+			{
+				eRead = ( 1 << 0 ),
+				eWrite = ( 1 << 1 ),
+			} ;
+
+			template <typename T> void	monitor( T, int amodes = eRead ) ;
 
 			void	destroy( void ) ;
 
@@ -31,13 +35,25 @@ namespace btl
 			bool	isactive(void) const { return ! stor_.empty() ; }
 
 		protected:
-			void	scan( int aactive ) ;
-			void	activity( int amask, io * ioptr ) ;
+			template <typename T> void	replace( T, int amodes = eRead ) ;
 
-			void	add( io *) ;
+		private:
+			struct concept_t {
+				virtual ~concept_t() = default ;
+
+				virtual bool	isactive_() const = 0 ;
+
+				virtual bool	doread_() const = 0 ;
+				virtual bool	dowrite_() const = 0 ;
+			} ;
+
+			template <typename T> struct adapter_t ;
+
+			void	scan( int aactive ) ;
+			void	activity( int amask, int aport ) ;
 
 			std::vector<pollfd>	poll_ ;
-			std::unordered_map<IO_Port, io *>	stor_ ;
+			std::unordered_map<IO_Port, std::unique_ptr<const concept_t>>	stor_ ;
 
 	} ;
 } ;
