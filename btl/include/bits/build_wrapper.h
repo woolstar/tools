53c0f372e111a70fa05cb3f45a851e8db86da162
woolstar <jdwoolverton@gmail.com>
2013-11-25 14:25:03 -0700

    change constructor to take buffer arg again

--- btl/include/bits/build_wrapper.h
+++ btl/include/bits/build_wrapper.h
@@ -9,7 +9,7 @@ namespace btl
 	{
 		public:
 			template <typename T>
-				build_if() : worker_( new adapter_t<T>()) { }
+				build_if(T x) : worker_( new adapter_t<T>(move( x))) { }
 
 				// standard build calls
 			build_if &	add(const buffer & abuf) { worker_->add_( abuf) ;  return * this ; }
@@ -45,6 +45,8 @@ namespace btl
 			template <typename T>
 				struct adapter_t : public concept_if
 				{
+					adapter_t( T && x) { buf_( move( x)) ; }
+
 					void add_(const buffer & abuf) { buf_.add( abuf) ; }
 					void add_(scanner<> abuf) { buf_.add( abuf) ; }
 					void add_(scanner<> abuf, size_t asz) { buf_.add( abuf, asz) ; }
