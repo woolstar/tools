766edb29f75a0ceed5dffb768dd226f89b7d2ba4
woolstar <jdwoolverton@gmail.com>
2013-12-12 10:40:57 -0700

    pointer hacking to work with const *

--- btl/data/build.cpp
+++ btl/data/build.cpp
@@ -10,18 +10,26 @@ build_base:: ~ build_base() { }
 void	build_base::copy(buffer const & abuf)
 {
 	scanner<sized_storage>	scan( abuf) ;
+	sized_storage * xptr= ( sized_storage *) far_ ;
 
-	while ( scan && ( fill_ != limit_ ) ) { *( fill_ ++)= * (scan ++ ) ; }
+	while ( scan && ( xptr != limit_ ) ) { *( xptr ++)= * (scan ++ ) ; }
+	far_= xptr ;
 }
 
 void	build_base::copy(scanner<> & ascan)
 {
-	while ( ascan && ( fill_ != limit_ ) ) { *( fill_ ++)= * (ascan ++ ) ; }
+	sized_storage * xptr= ( sized_storage *) far_ ;
+
+	while ( ascan && ( xptr != limit_ ) ) { *( xptr ++)= * (ascan ++ ) ; }
+	far_= xptr ;
 }
 
 void	build_base::copy(scanner<> & ascan, size_t asz)
 {
-	while ( ascan && asz && ( fill_ != limit_ ) ) { *( fill_ ++)= * (ascan ++ ) ;  -- asz ; }
+	sized_storage * xptr= ( sized_storage *) far_ ;
+
+	while ( ascan && asz && ( xptr != limit_ ) ) { *( xptr ++)= * (ascan ++ ) ;  -- asz ; }
+	far_= xptr ;
 }
 
 void	build_base::print(const char * afmt, ...)
@@ -30,7 +38,7 @@ void	build_base::print(const char * afmt, ...)
 	int iret ;
 
 	va_start(vl, afmt) ;
-	iret= vsnprintf(reinterpret_cast<char *>( fill_), remaining(), afmt, vl) ;
+	iret= vsnprintf((char *) far_, remaining(), afmt, vl) ;
 	va_end( vl) ;
 
 	if ( iret > 0 ) { jump( iret ) ; }
