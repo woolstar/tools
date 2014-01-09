f2e76781254f3be5337895fdf34c2d7185a11563
woolstar <jdwoolverton@gmail.com>
2014-01-08 22:44:50 -0700

    include memory and define constructors

--- ctl/include/bits/vec_base.h
+++ ctl/include/bits/vec_base.h
@@ -1,16 +1,29 @@
+// vec_base -*- C++ -*-
+
+#ifndef	_CUDETAIL_VEC_BASE
+#define _CUDETAIL_VEC_BASE
+
+#include <memory>
+
 namespace ctl
 {
 	namespace __detail
 	{
 		class vector_base
 		{
-			void	size(size_t) ;
-			void	shrink(size_t) ;
+			public:
+				vector_base() { }
+				vector_base(size_t asize) : storage_( new unsigned char[ asize ] ), total_( asize ) { }
 
-				// storage
-			size_t	use_ = 0, total_ = 0 ;
-			std::unique_ptr<unsigned char []>	storage_ ;
+				void	size(size_t) ;
+				void	shrink(size_t) ;
+
+					// storage
+				size_t	use_ = 0, total_ = 0 ;
+				std::unique_ptr<unsigned char []>	storage_ ;
 		} ;
 	} ;
 } ;
+
+#endif
 
