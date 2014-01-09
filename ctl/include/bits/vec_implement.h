8ae10fb16653eefe832847daaaa1cf6c931bf684
woolstar <jdwoolverton@gmail.com>
2014-01-08 21:15:58 -0700

    implementation of vector methods

---
+++ ctl/include/bits/vec_implement.h
@@ -0,0 +1,15 @@
+
+namespace ctl
+{
+
+	// implementation
+	// lots of ugly
+
+	template <class T>
+		template <class Tc, class... Args>
+			void vector<T>::emplace_back( Args&&... arg )
+			{
+			}
+
+} ;
+
