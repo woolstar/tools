a900b110a6bb2857104741f67cda9a58a5c4641a
woolstar <jdwoolverton@gmail.com>
2014-01-10 16:08:31 -0700

    define test class points

--- demo/demo_cvec.cpp
+++ demo/demo_cvec.cpp
@@ -2,10 +2,13 @@
 #include <io>
 
 using btl::ioout ;
+using btl::ioerr ;
 
 	class	Base
 	{
 		public:
+			Base() { ioerr << "C base\n" ; }
+
 			virtual ~ Base() = 0 ;
 			virtual void  action() = 0 ;
 			virtual void  show() = 0 ;
@@ -14,8 +17,8 @@ using btl::ioout ;
 	class	test1 : public Base
 	{
 		public:
-			test1(int aval) : m_val( aval) { }
-			~ test1() ;
+			test1(int aval) : m_val( aval) { ioerr << "C test1 <int>\n" ; }
+			~ test1() { ioerr << "D test1\n" ; }
 
 			void	action() override { m_val ++ ; }
 			void	show() override { ioout << btl::format("%8d ", m_val) ; }
@@ -23,9 +26,11 @@ using btl::ioout ;
 			int	m_val ;
 	} ;
 
+	Base:: ~ Base() { }
+
 int main()
 {
-	ctl::vector<Base>	test ;
+	ctl::vector<Base>	test(1024) ;
 
 	test.emplace_back<test1>(10) ;
 
