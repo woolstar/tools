7b4c867e77cc4dc43ec4be6e999bc053a03070a6
woolstar <jdwoolverton@gmail.com>
2014-01-12 23:13:10 -0700

    add in skeleton for iter

---
+++ ctl/include/bits/tank_iter.h
@@ -0,0 +1,21 @@
+// tank_iter.h -*- C++ -*-
+
+#ifndef _CTL_TANKITER
+#define _CTL_TANKITER	1
+
+namespace ctl
+{
+
+	template<class T>
+		class tank<T>::iterator : public __detail::tank_iter_b
+		{
+			public:
+				iterator(data *)
+				{
+				}
+		} ;
+
+} ;
+
+#endif
+
