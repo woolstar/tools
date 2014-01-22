89f74ee9ba409151ba4e10178399fca2c8c93c38
woolstar <jdwoolverton@gmail.com>
2014-01-21 23:51:36 -0700

    test harness for ctl::vector

---
+++ demo/demo_cvek.cpp
@@ -0,0 +1,36 @@
+#include <polyco>
+#include <io>
+#include <iterator>
+
+using btl::ioout ;
+using btl::ioerr ;
+
+#include "check.cpp"
+#include "ctltypes.h"
+
+	////
+
+	void show( const ctl::vector<Base> & atk ) { for ( const auto & br : atk ) { br.show() ; } }
+
+	void ctest( const ctl::vector<Base> & atk )
+	{
+		auto ir= atk.span() ;
+
+		if ( ir ) { ir-> show() ; }
+	}
+
+int main()
+{
+	ctl::vector<Base>	test(1024) ;
+
+	check( test.empty(), true ) ; 
+	check( test.capacity(), 1024 ) ;
+
+	ioerr << "---------\n" ;
+
+	fprintf(stderr, "passed %d tests.\n", _passed) ;
+	ctl::vector<Base>::dotrace() ;
+
+	return 0 ;
+}
+
