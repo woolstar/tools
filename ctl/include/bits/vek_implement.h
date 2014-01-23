7da147a7a2dd8d1351c7a4c24d2ff3a21892f3e1
woolstar <jdwoolverton@gmail.com>
2014-01-23 10:00:06 -0700

    add in layout of vector_ctrl

--- ctl/include/bits/vek_implement.h
+++ ctl/include/bits/vek_implement.h
@@ -11,6 +11,38 @@ namespace ctl
 {
 	namespace __detail
 	{
+		template <class T, class Tb>
+			class vector_ctrl : public vector_ctrl_common<Tb>
+			{
+				public:
+					vector_ctrl( vector_ctrl && ) ;
+					template <class... Ar>
+						vector_ctrl( Ar&&... arg ) : vector_ctrl()
+						{
+							void * mem= & storage_ ;
+							tank_ctrl_common<Tb>::locate( new( mem)T(arg... ) ) ;
+						}
+
+					using data = tank_ctrl_base::data ;
+
+					void move( data * zstorage ) override { }
+					void destroy(void) override { ptr()-> ~ T() ; }
+					void trace(void) const override
+					{
+					} ;
+
+				protected:
+					vector_ctrl() : vector_ctrl_common<Tb>( (long) &storage_ - (long) this ) { }
+
+					friend class vector<Tb> ;
+
+				private:
+					T * ptr(void) noexcept
+						{ return static_cast<T *>( (void *) &storage_ ) ; }
+
+					typename std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type
+						storage_ ;
+			} ;
 	} ;
 
 	template <class T>
