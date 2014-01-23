a8db13ad9cfd18fc7cdf6e393b7114a147a010e6
woolstar <jdwoolverton@gmail.com>
2014-01-22 21:54:35 -0700

    vector template method implementations

---
+++ ctl/include/bits/vek_implement.h
@@ -0,0 +1,22 @@
+// vek_implement.h -*- C++ -*-
+
+#ifndef _CTL_VECTORIMPL
+#define _CTL_VECTORIMPL	1
+
+#include <typeinfo>
+#include <utility>
+#include <cstring>
+
+namespace ctl
+{
+	namespace __detail
+	{
+	} ;
+
+	template <class T>
+		vector::vector(size_t asize)
+		{
+			// guess at how large N Base are going to be + ctrl ;
+		}
+} ;
+
