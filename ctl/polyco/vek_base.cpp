adff51483738c3518152e14d47ae20b179f55c53
woolstar <jdwoolverton@gmail.com>
2014-01-23 12:19:18 -0700

    implement split reserve

--- ctl/polyco/vek_base.cpp
+++ ctl/polyco/vek_base.cpp
@@ -15,6 +15,24 @@ void	vector_base::clear( void )
 {
 }
 
+void vector_base::reserve(size_t dsize, size_t nsize )
+{
+	size_t mtot= use_ + dsize ;
+
+	if ( mtot > total_ )
+	{
+		size_t mgrow= 1.2 * total_ ;
+		if ( mtot > mgrow ) { mgrow= mtot ; }
+		unsigned char * pnew = new unsigned char [mgrow] ;
+		
+		relocate( storage_.get(), storage_.get() + use_, pnew ) ;
+		storage_.reset( pnew) ;
+		total_ = mgrow ;
+	}
+
+	offsets_.reserve( nsize ) ;
+}
+
 void vector_base::shrink( size_t )
 {
 }
